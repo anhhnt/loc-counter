79f2396535e1b309e9863ff19f15d9583f70d000
Anh Hoang <ntahoang@gmail.com>
2016-06-01 23:27:25 +0700

    Read rev, author, date, loc from saved file

--- cr.py
+++ cr.py
@@ -1,14 +1,39 @@
 import re
+import time
+from dateutil.parser import parse
 
-def process(line):
-	if 'Sent:' in line:
-		print(line)
-	elif 'Subject:' in line:
-		matchObj = re.search(r'secutix2\-[a-z]*: svn commit \(([a-z]{3})\)', line, re.I)
-		if matchObj:
-			print(matchObj.group(1))
-		else:
-			print("No match!!" + line)
+def readValue(file, pattern):
+	while True:
+		line = file.readline()
+		matchedText = re.search(pattern, line)
+		if matchedText != None:
+			print(matchedText.group(0))
+			return matchedText.group(0)
+
+def countLoc(file):
+	loc = 0
+	while True:
+		line = file.readline()
+		if re.match(r'^\+[^\+]{2}.*$', line, re.MULTILINE):
+			loc += 1
+		elif 'ELCA Subversion service' in line:
+			break
+	return loc
+
 with open('./reviewed.txt') as f:
-	for line in f:
-		process(line)
+	while True:
+		line = f.readline()
+		if line == '':
+			break
+		if re.search(r'^Revision$', line) != None:
+			revision = readValue(f, r'(\d*)')
+		elif re.search(r'^Author$', line) != None:
+			author = readValue(f, r'([a-z]{3})')
+		elif re.search(r'^Date$', line) != None:
+			date = readValue(f, r'^(.*)$')
+			b = parse(date[:20])
+			#print(str(b.day) + '/' + str(b.month) + '/' + str(b.year))
+			realDate = time.strptime(str(b.day) + '/' + str(b.month) + '/' + str(b.year), "%d/%m/%Y")
+		elif re.search(r'^Diff$', line) != None:
+			loc = countLoc(f)
+			print(loc)
